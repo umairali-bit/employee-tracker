af450183780375dd181f6a6ac1ed1117c344c67e
Umair Ali <umairmamoor@gmail.com>
2021-09-08 04:15:13 +0500

    inital files

---
+++ index.js
@@ -0,0 +1,5 @@
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+//setu-up inquirer (choices) to present the prompts
+
+//setup switch statements each case in switch statment will be one of the choices. each case should have its own function. that function then will use a different query. each function needs to be individually defined. 
