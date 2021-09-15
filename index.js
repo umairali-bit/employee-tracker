f8e3b37eab5bee269b1d8d0a3cb5ade1d53a5c5d
Umair Ali <umairmamoor@gmail.com>
2021-09-15 12:34:32 -0400

    inquirer function that asks all the prompts from the user

--- index.js
+++ index.js
@@ -4,7 +4,52 @@
 
 //setup switch statements each case in switch statment will be one of the choices. each case should have its own function. that function then will use a different query. each function needs to be individually defined. 
 
-
+const { prompt } = require("inquirer");
 const logo = require('asciiart-logo');
 const config = require('./package.json');
+require("console.table");
 console.log(logo(config).render());
+
+
+// Inquirer function
+
+function mainPrompts() {
+    prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "View_Employees"
+                },
+                {
+                    name: "View All Employees By Department",
+                    value: "View_Employees_By_Department"
+                },
+                {
+                    name: "View All Employees By Manager",
+                    value: "View_Employees_By_Manager"
+                },
+                {
+                    name: "Add an Employee",
+                    value: "Add_Employee"
+                },
+                {
+                    name: "Remove an Employee",
+                    value: "Remove_Employee"
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "Update_Employee"
+                },
+                {
+                    name: "Update Employee Manager",
+                    value: "Update_Manager"
+                }
+            ]
+        }
+    ])
+}
+mainPrompts();
