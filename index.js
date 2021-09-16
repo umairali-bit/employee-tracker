8a0dbe0a6056cf91f1eb967ddb4d62d5cf4ddd3b
Umair Ali <umairmamoor@gmail.com>
2021-09-15 20:03:20 -0400

    ViewAllEmployees function to show all the employees

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //setu-up inquirer (choices) to present the prompts
 
 //setup switch statements each case in switch statment will be one of the choices. each case should have its own function. that function then will use a different query. each function needs to be individually defined. 
-
+const db = require('./db/connection');
 const { prompt } = require("inquirer");
 const logo = require('asciiart-logo');
 const config = require('./package.json');
@@ -19,37 +19,32 @@ function mainPrompts() {
             type: "list",
             name: "choice",
             message: "What would you like to do",
-            choices: [
-                {
-                    name: "View All Employees",
-                    value: "View_Employees"
-                },
-                {
-                    name: "View All Employees By Department",
-                    value: "View_Employees_By_Department"
-                },
-                {
-                    name: "View All Employees By Manager",
-                    value: "View_Employees_By_Manager"
-                },
-                {
-                    name: "Add an Employee",
-                    value: "Add_Employee"
-                },
-                {
-                    name: "Remove an Employee",
-                    value: "Remove_Employee"
-                },
-                {
-                    name: "Update Employee Role",
-                    value: "Update_Employee"
-                },
-                {
-                    name: "Update Employee Manager",
-                    value: "Update_Manager"
-                }
-            ]
+            choices: [ 'View All Employees', 'View All Departments', 'View All Roles','View Employees by Manager', 'View Employees by Department','Add a Department', 'Add a Role', 
+                       'Add an Employee', 'Update an Employee Role', 'Update an Employee Manager', 'Delete Employee','Delete Role',
+                       'Delete Department'],
         }
-    ])
-}
+    ]).then((answers) => {
+        
+        if (answers.choice === 'View All Employees') {
+            viewAllEmployees();
+        }
+
+
+
+});
+};
+function viewAllEmployees(){
+    const sql = `SELECT * FROM employees`;
+    db.query(sql, (err, result) => {
+        if (err) {
+        res.status(400).json({ error: res.message });
+        }
+        console.table(result);
+        mainPrompts();
+        
+    })
+};
+
 mainPrompts();
+
+
